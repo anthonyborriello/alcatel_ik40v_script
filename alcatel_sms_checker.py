eb7d0bd3b0ca640474830667fc074e8ad60787f5
Antonio Borriello <57049017+anthonyborriello@users.noreply.github.com>
2025-02-13 22:34:32 +0100

    Update alcatel_sms_checker.py

--- alcatel_sms_checker.py
+++ alcatel_sms_checker.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Alcatel SMS Storage Checker
+# Copyright (c) 2024 Anthony Borriello
+#
+# This script retrieves and analyzes the SMS storage status of an Alcatel device via its web API.
+# Licensed under the MIT License.
+#
+# GitHub Repository: https://github.com/anthonyborriello/alcatel_ik40v_script
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# See the LICENSE file for more details.
+
 import requests
 
 # Device URL
